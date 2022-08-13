95dcde3dfa06f76be509bfc465da89ec329b35b1
ThiagoFTelles <atendimento.telles@gmail.com>
2022-08-12 23:00:02 -0300

    refactor: change import

--- src/presentation/helpers/http-helper.ts
+++ src/presentation/helpers/http-helper.ts
@@ -1,6 +1,5 @@
-import { MissingParamError } from "../errors/missing-param-error"
-import { ServerError } from "../errors/server-error"
-import { HttpResponse } from "../protocols/http"
+import { ServerError } from "../errors"
+import { HttpResponse } from "../protocols"
 
 export const badRequest = (error: Error): HttpResponse => {
   return {
