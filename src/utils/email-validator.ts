8eaa302ed8542f37ff5aecbc78d5746fd57a6195
ThiagoFTelles <atendimento.telles@gmail.com>
2022-08-23 14:30:41 -0300

    feat: ensure EmailValidatorAdpter return true if EmailValidatorReturns true

--- src/utils/email-validator.ts
+++ src/utils/email-validator.ts
@@ -1,7 +1,14 @@
 import { EmailValidator } from "../presentation/protocols/email-validator";
+import validator from "validator";
 
 export class EmailValidatorAdapter implements EmailValidator {
   isValid(email: string): boolean {
-    return false
+    return validator.isEmail(email)
   }
 }
+// TODO: quando a internet voltar: npm i @types/validator -D
+// Parei na aula criando email validator adapter 17:16
+// TODO: fazer o outro commit em 14:14
+// 15:27 const makeSut = (): EmailValidatorAdapter => {
+//   return new EmailValidatorAdapter()
+// }
