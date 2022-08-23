18a6e8f8e6d829e5f0bd5ffc08626055fb5f094d
ThiagoFTelles <atendimento.telles@gmail.com>
2022-08-23 14:35:54 -0300

    feat: install @types/validator

--- src/utils/email-validator-adapter.ts
+++ src/utils/email-validator-adapter.ts
@@ -6,5 +6,3 @@ export class EmailValidatorAdapter implements EmailValidator {
     return validator.isEmail(email)
   }
 }
-// TODO: quando a internet voltar: npm i @types/validator -D
-// Parei na aula criando email validator adapter 
